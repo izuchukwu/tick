c0da10fda38287cc228d9d7637186f04799ea721
Izuchukwu Elechi <izuchukwuelechi@gmail.com>
2015-06-20 00:27:26 -0500

    Xcode project gen & pod install

---
+++ Tick/Tick/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tick
+//
+//  Created by Izuchukwu Elechi on 6/20/15.
+//  Copyright (c) 2015 Izuchukwu Elechi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tick/Tick/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tick
+//
+//  Created by Izuchukwu Elechi on 6/20/15.
+//  Copyright (c) 2015 Izuchukwu Elechi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
